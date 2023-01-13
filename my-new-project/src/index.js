b74749a6209abe4d47e79847bd0beb6eeb77a15e
Augusto Leal <aleal4@gmail.com>
2023-01-12 23:12:45 -0700

    fixed docker error

--- my-new-project/src/index.js
+++ my-new-project/src/index.js
@@ -1,21 +1,17 @@
-const express = require('express')
-const fs = require('fs')
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
-const app = express()
+const app = express();
+const port = 3000;
 
-if (!process.env.PORT) {
-    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT")
-}
-const PORT = process.env.PORT;
-
+//
+// Registers a HTTP GET route for video streaming.
+//
 app.get("/video", (req, res) => {
 
-    //
-    // Original video from here:
-    // https://sample-videos.com
-    //
-    const path = "./videos/SampleVideo_1280x720_1mb.mp4";
-    fs.stat(path, (err, stats) => {
+    const videoPath = path.join("./videos", "SampleVideo_1280x720_1mb.mp4");
+    fs.stat(videoPath, (err, stats) => {
         if (err) {
             console.error("An error occurred ");
             res.sendStatus(500);
@@ -26,13 +22,13 @@ app.get("/video", (req, res) => {
             "Content-Length": stats.size,
             "Content-Type": "video/mp4",
         });
-        fs.createReadStream(path).pipe(res);
+        fs.createReadStream(videoPath).pipe(res);
     });
 });
 
 //
 // Starts the HTTP server.
 //
-app.listen(PORT, () => {
-    console.log(`Microservice listening on port ${PORT}, point your browser at http://localhost:3000/video`);
+app.listen(port, () => {
+    console.log(`Microservice listening on port ${port}, point your browser at http://localhost:3000/video`);
 });
