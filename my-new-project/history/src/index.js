094df233b09f24fb2fb5dbf78466d2440a836c9c
Augusto Leal <aleal4@gmail.com>
2023-01-14 12:19:31 -0700

    starting history microservice

---
+++ my-new-project/history/src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+//
+// Setup event handlers.
+//
+function setupHandlers(app) {
+    // ... THIS IS A STUB MICROSERVICE: SETUP YOUR HTTP ROUTES HERE ...
+}
+
+//
+// Start the HTTP server.
+//
